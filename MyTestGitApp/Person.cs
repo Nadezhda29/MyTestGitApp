e08ca390f4c245674b4ab9d86aa71c6351c3d539
Надежда Острожинская <nadegda9966n@gmail.com>
2021-10-07 22:18:40 +0700

    add Person

---
+++ MyTestGitApp/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTestGitApp
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Say()
+        {
+            Console.WriteLine("Привет!");
+        }
+    }
+}
