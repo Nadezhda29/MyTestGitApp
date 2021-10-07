09d3a0746674c4695fb2f2697e5e78d9c46cb2af
Надежда Острожинская <nadegda9966n@gmail.com>
2021-10-07 22:33:23 +0700

    person update

--- MyTestGitApp/Person.cs
+++ MyTestGitApp/Person.cs
@@ -9,6 +9,7 @@ namespace MyTestGitApp
         public int ID { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        public string Address { get; set; }
 
         public void Say()
         {
